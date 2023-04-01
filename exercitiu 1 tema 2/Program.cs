2d36ef202a658bd83758a5a2a7e60d6d54c17508
Daniel Codreanu <danielcdr2006@gmail.com>
2023-04-01 23:46:41 +0300

    Add project files.

---
+++ exercitiu 1 tema 2/Program.cs
@@ -0,0 +1,30 @@
+﻿
+using System;
+
+namespace MyFirstProgram2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+
+            while (n != 0)
+            {
+
+                int cifra = n % 10;
+                sum = sum + cifra;
+                n = n / 10;
+
+
+
+            }
+            Console.WriteLine(sum);
+            Console.ReadLine();
+
+        }
+    }
+}
